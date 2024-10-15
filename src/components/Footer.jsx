70b5f7c1d432d952e21180eaaeb3815ac5b6ac52
Your Name <you@example.com>
2024-10-15 05:28:51 -0700

    hello

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
               </li>
               <li>
                 <a href="#" className="hover:underline">
-                  Service
+                  Services
                 </a>
               </li>
               <li>
